86ceaa03f4e82782d80dc47cc660ad1e5b197e7d
LeoJulio <leonildojuliojulio@gmail.com>
2021-12-18 17:17:35 +0200

    adicao de funcionalidades no JS, adicao de mais elementos no index.html e estilizacao de tabela de resultados

--- js/main.js
+++ js/main.js
@@ -1,22 +1,69 @@
 const cards = document.querySelectorAll(".cards")
+var hTrabalho = document.querySelector(".hTrabalho")
 var input1 = document.querySelector(".inputCard1")
 var input2 = document.querySelector(".inputCard2")
 var input3 = document.querySelector(".inputCard3")
 var input4 = document.querySelector(".inputCard4")
+var input5 = document.querySelector(".inputCard5")
+var nClientesPD = document.querySelector(".nClientesPD")
+var nClientesPB = document.querySelector(".nClientesPB")
+var lucroBDia = document.querySelector(".lucroBDia")
+var lucroBDiaB = document.querySelector(".lucroBDiaB")
+var lucroBMes = document.querySelector(".lucroBMes")
+var lucroBAno = document.querySelector(".lucroBAno")
 
 
-if(input1 != null, input2 != null, input3 != null, input4 != null){
+/*if(input1 != null, input2 != null, input3 != null, input4 != null){
     input1.value
     input2.value
     input3.value
     input4.value
-}
+}*/
 
 var results = document.querySelector(".results")
 var btn1 = document.querySelector(".btnInput1")
 var btn2 = document.querySelector(".btnInput2")
 var btn3 = document.querySelector(".btnInput3")
 var btn4 = document.querySelector(".btnInput4")
+var btn5 = document.querySelector(".btnInput5")
+
+
+function passarHTrabalho(){
+    let valor = input3.value
+    hTrabalho.innerHTML = valor
+}
+
+function calcClientes(){
+    var valorInput1 = input1.value
+    var valorInput2 = input2.value
+    var valorInput3 = input3.value
+    var valorInput4 = input4.value
+    var minutosDeTrabalho = valorInput3 * 60
+
+    var nClientesPBalcao = Math.round((minutosDeTrabalho - valorInput4) / valorInput2)
+    var nClientesPDia = Math.round(nClientesPBalcao * valorInput1)
+
+    nClientesPD.innerHTML = nClientesPDia
+    nClientesPB.innerHTML = nClientesPBalcao
+
+    
+        var valorInput5 = input5.value
+    
+        var lucroDiario = nClientesPDia * valorInput5
+        var lucroDiarioBalcao = nClientesPBalcao * valorInput5
+        var lucroMensal = lucroDiario * 30
+        var lucroAnual = lucroMensal * 12
+
+            lucroBDia.innerHTML = lucroDiario + " Mts"
+            lucroBDiaB.innerHTML = lucroDiarioBalcao + " Mts"
+            lucroBMes.innerHTML = lucroMensal + " Mts"
+            lucroBAno.innerHTML = lucroAnual + " Mts"
+        
+    
+    
+}
+
+
 
 function indexActiveCard(){
     if(cards[0].classList.contains("activeCard")){
@@ -30,6 +77,9 @@ function indexActiveCard(){
     }
     if(cards[3].classList.contains("activeCard")){
         return 3
+    }
+    if(cards[4].classList.contains("activeCard")){
+        return 4
     }
 }
 
@@ -76,13 +126,34 @@ btn4.addEventListener("click", ()=>{
     if(input4.value == ""){
         alert("Preencha o campo em branco antes de seguir.")
     }else{
+        var n = indexActiveCard() 
+    + 1
+        console.log(indexActiveCard())
         cards[indexActiveCard()].classList.remove("activeCard")
-        results.style.display = "block"
-        results.classList.add("activeCard")
+        cards[n].classList.add("activeCard")
     }
 
     
 })
+
+btn5.addEventListener("click", ()=>{
+    if(input5.value == ""){
+        alert("Preencha o campo em branco antes de seguir.")
+    }else{
+        cards[indexActiveCard()].classList.remove("activeCard")
+        results.style.display = "block"
+        results.classList.add("activeCard")
+
+        passarHTrabalho()
+        calcClientes()
+    }
+
+    
+})
+
+
+
+
 
 
 
